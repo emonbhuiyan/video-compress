dffbdbbe69a2af1e207ac009e0541fd44ab41174
Emon Bhuiyan <mdemonbhuiyan@live.com>
2026-03-20 16:25:05 +0300

    Github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+//   server: {
+//     headers: {
+//       'Cross-Origin-Embedder-Policy': 'require-corp',
+//       'Cross-Origin-Opener-Policy': 'same-origin',
+//     },
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  // This tells Vite your site is at /video-compress/ instead of the root
+  base: '/video-compress/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
