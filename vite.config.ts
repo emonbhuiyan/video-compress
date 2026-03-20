1f1fb57caa82b7268a5cac339d8d6a354737e509
Emon Bhuiyan <mdemonbhuiyan@live.com>
2026-03-20 16:37:06 +0300

    Config Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,8 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import path from 'path';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     exclude: ['lucide-react'],
-//   },
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, './src'),
-//     },
-//   },
-//   server: {
-//     headers: {
-//       'Cross-Origin-Embedder-Policy': 'require-corp',
-//       'Cross-Origin-Opener-Policy': 'same-origin',
-//     },
-//   },
-// });
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  // This tells Vite your site is at /video-compress/ instead of the root
-  base: '/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
@@ -43,3 +19,27 @@ export default defineConfig({
     },
   },
 });
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+
+// export default defineConfig({
+//   // This tells Vite your site is at /video-compress/ instead of the root
+//   base: '/', 
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+//   server: {
+//     headers: {
+//       'Cross-Origin-Embedder-Policy': 'require-corp',
+//       'Cross-Origin-Opener-Policy': 'same-origin',
+//     },
+//   },
+// });
